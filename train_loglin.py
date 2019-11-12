70e2f92f30c1bcc26340dea03fa0e13190055e97
Lidor115 <lidor115@gmail.com>
2019-11-12 17:02:30 +0200

    train_loglin

--- train_loglin.py
+++ train_loglin.py
@@ -1,22 +1,28 @@
 import loglinear as ll
 import numpy as np
 import random
-
+import utils as ut
 STUDENT = {'name': 'Tal Levy',
            'ID': '---'}
+def construct_dict_encoders(unique_items):
+    item2idx = dict((item, index) for index, item in enumerate(unique_items))
+    idx2item = dict((index, item) for index, item in enumerate(unique_items))
+    return item2idx, idx2item
 
 
-def feats_to_vec(features):
-    # YOUR CODE HERE.
-    # Should return a numpy vector of features.
-    return None
+def feats_to_vec(features, f2I):
+    x_vec  = np.zeros(len(f2I))
+    for f in features:
+        if f in f2I:
+            x_vec[f2I[f]] +=1
+    return x_vec
 
 
-def accuracy_on_dataset(dataset, params):
+def accuracy_on_dataset(dataset, params,f2I,l2I):
     good = bad = 0.0
     for label, features in dataset:
-        x = feats_to_vec(features)
-        y = label
+        x = feats_to_vec(features,f2I)
+        y = l2I[label]
         pred = ll.predict(x, params)
         if pred == y:
             good += 1
@@ -25,7 +31,7 @@ def accuracy_on_dataset(dataset, params):
     return good / (good + bad)
 
 
-def train_classifier(train_data, dev_data, num_iterations, learning_rate, params):
+def train_classifier(train_data, dev_data, num_iterations, learning_rate, params, f2I,l2I):
     """
     Create and train a classifier, and return the parameters.
 
@@ -40,8 +46,8 @@ def train_classifier(train_data, dev_data, num_iterations, learning_rate, params
         cum_loss = 0.0 # total loss in this iteration.
         random.shuffle(train_data)
         for label, features in train_data:
-            x = feats_to_vec(features) # convert features to a vector.
-            y = label                  # convert the label to number if needed.
+            x = feats_to_vec(features,f2I) # convert features to a vector.
+            y = l2I[label]                  # convert the label to number if needed.
             loss, grads = ll.loss_and_gradients(x, y, _params)
             cum_loss += loss
             W, b = _params
@@ -51,8 +57,8 @@ def train_classifier(train_data, dev_data, num_iterations, learning_rate, params
             _params = [W, b]
 
         train_loss = cum_loss / len(train_data)
-        train_accuracy = accuracy_on_dataset(train_data, params)
-        dev_accuracy = accuracy_on_dataset(dev_data, params)
+        train_accuracy = accuracy_on_dataset(train_data, params,f2I,l2I)
+        dev_accuracy = accuracy_on_dataset(dev_data, params,f2I,l2I)
         print(I, train_loss, train_accuracy, dev_accuracy)
     return _params
 
@@ -63,7 +69,16 @@ if __name__ == '__main__':
     # and call train_classifier.
     
     # ...
-   
+
+    train_data = ut.TRAIN
+    l2I = ut.L2I
+    f2I = ut.F2I
+    dev_data = ut.DEV
+    in_dim = len(ut.vocab)
+    out_dim = len(l2I)
+    num_iterations =1
+    learning_rate = 0.001
+
     params = ll.create_classifier(in_dim, out_dim)
-    trained_params = train_classifier(train_data, dev_data, num_iterations, learning_rate, params)
+    trained_params = train_classifier(train_data, dev_data, num_iterations, learning_rate, params, f2I,l2I)
 
