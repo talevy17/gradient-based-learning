f02de4a06c1afcbcbb684ba876500d61370c63b0
Tal Levy <tall@wix.com>
2019-11-12 10:54:07 +0200

    initial file uploading

---
+++ mlp1.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+STUDENT={'name': 'YOUR NAME',
+         'ID': 'YOUR ID NUMBER'}
+
+def classifier_output(x, params):
+    # YOUR CODE HERE.
+    return probs
+
+def predict(x, params):
+    """
+    params: a list of the form [W, b, U, b_tag]
+    """
+    return np.argmax(classifier_output(x, params))
+
+def loss_and_gradients(x, y, params):
+    """
+    params: a list of the form [W, b, U, b_tag]
+
+    returns:
+        loss,[gW, gb, gU, gb_tag]
+
+    loss: scalar
+    gW: matrix, gradients of W
+    gb: vector, gradients of b
+    gU: matrix, gradients of U
+    gb_tag: vector, gradients of b_tag
+    """
+    # YOU CODE HERE
+    return ...
+
+def create_classifier(in_dim, hid_dim, out_dim):
+    """
+    returns the parameters for a multi-layer perceptron,
+    with input dimension in_dim, hidden dimension hid_dim,
+    and output dimension out_dim.
+
+    return:
+    a flat list of 4 elements, W, b, U, b_tag.
+    """
+    params = []
+    return params
+
