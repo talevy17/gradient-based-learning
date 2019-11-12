96c1285f96ecd834b791d16b6265df88bbdb415f
Tal Levy <tall@wix.com>
2019-11-12 16:19:10 +0200

    linear train update

--- train_loglin.py
+++ train_loglin.py
@@ -1,4 +1,5 @@
 import loglinear as ll
+import numpy as np
 import random
 
 STUDENT = {'name': 'Tal Levy',
@@ -32,23 +33,26 @@ def train_classifier(train_data, dev_data, num_iterations, learning_rate, params
     learning_rate: the learning rate to use.
     params: list of parameters (initial values)
     """
+    _params = params
     for I in range(num_iterations):
         cum_loss = 0.0 # total loss in this iteration.
         random.shuffle(train_data)
         for label, features in train_data:
             x = feats_to_vec(features) # convert features to a vector.
             y = label                  # convert the label to number if needed.
-            loss, grads = ll.loss_and_gradients(x,y,params)
+            loss, grads = ll.loss_and_gradients(x, y, _params)
             cum_loss += loss
-            # YOUR CODE HERE
-            # update the parameters according to the gradients
-            # and the learning rate.
+            W, b = _params
+            Wg, bg = grads
+            W -= np.dot(learning_rate, Wg)
+            b -= np.dot(learning_rate, bg)
+            _params = [W, b]
 
         train_loss = cum_loss / len(train_data)
         train_accuracy = accuracy_on_dataset(train_data, params)
         dev_accuracy = accuracy_on_dataset(dev_data, params)
         print(I, train_loss, train_accuracy, dev_accuracy)
-    return params
+    return _params
 
 
 if __name__ == '__main__':
